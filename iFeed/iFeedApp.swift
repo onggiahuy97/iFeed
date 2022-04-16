63baff7ffe13665113cb1864893d2158d14c8d4a
Huy Ong <ogh.huy@gmail.com>
2022-04-16 16:52:23 -0700

    Initial Commit

---
+++ iFeed/iFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFeedApp.swift
+//  iFeed
+//
+//  Created by Huy Ong on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct iFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
